2f55dfbb12b6759b69ce9cf2c78b7866a25cbcd2
Alexander Dodatko <alexander.dodatko@dataart.com>
2016-05-11 12:59:02 +0300

    added framework targets

---
+++ AmberFoundation/AmberFoundation-dynamic/AmberFoundation-dynamic.h
@@ -0,0 +1,19 @@
+//
+//  AmberFoundation-dynamic.h
+//  AmberFoundation-dynamic
+//
+//  Created by Alexander Dodatko on 5/11/16.
+//  Copyright © 2016 EPAM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AmberFoundation-dynamic.
+FOUNDATION_EXPORT double AmberFoundation_dynamicVersionNumber;
+
+//! Project version string for AmberFoundation-dynamic.
+FOUNDATION_EXPORT const unsigned char AmberFoundation_dynamicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AmberFoundation_dynamic/PublicHeader.h>
+
+
---
+++ AmberFoundation/AmberFoundation-static/AmberFoundation-static.h
@@ -0,0 +1,19 @@
+//
+//  AmberFoundation-static.h
+//  AmberFoundation-static
+//
+//  Created by Alexander Dodatko on 5/11/16.
+//  Copyright © 2016 EPAM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AmberFoundation-static.
+FOUNDATION_EXPORT double AmberFoundation_staticVersionNumber;
+
+//! Project version string for AmberFoundation-static.
+FOUNDATION_EXPORT const unsigned char AmberFoundation_staticVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AmberFoundation_static/PublicHeader.h>
+
+
